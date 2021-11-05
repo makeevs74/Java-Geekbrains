e15dc19319228ba561d0b63e1f0d54487610f968
makeevs74 <makeevs74@gmai.com>
2021-11-05 23:51:51 +0500

    Rebuilt Exercise three

---
+++ HomeWorkTwo.java
@@ -0,0 +1,12 @@
+/**
+  * Java. Homework 2
+  *
+  * @author Makeev Sergey
+  * @version ???.11.2021
+  */
+
+public class HomeWorkTwo {
+    public static void main(String[] args) {
+        
+    }
+}
