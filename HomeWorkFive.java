110785a5b339baf94205ec7a138cbca483a5ff51
makeevs74 <82934913+makeevs74@users.noreply.github.com>
2021-11-19 00:13:39 +0500

    Update HomeWorkFive.java

--- HomeWorkFive.java
+++ HomeWorkFive.java
@@ -6,16 +6,22 @@
   */
 class HomeWorkFive {
     public static void main(String[] args) {
-        Employee[] employeeList = new Employee[5];
-        employeeList[0] = new Employee("Maria Popova", "Secretary", "maria96@mail.ru", "8-999-333-4554", 27000, 25);
-        employeeList[1] = new Employee("Artem Fedorov", "Lawyer", "afedorov@mail.ru", "8-922-111-8787", 40000, 27);
-        employeeList[2] = new Employee("Stanislav Sergeev", "Manager", "sergeev_stas@mail.ru", "8-912-322-6754", 32000, 23);
-        employeeList[3] = new Employee("Aleksander Smirnov", "Senior manager", "SmirnovAA@gmail.ru", "8-999-777-8888", 50000, 37);
-        employeeList[4] = new Employee("Igor Davidov", "Head of sales department", "DavidovIV76@mail.ru", "8-919-565-7878", 70000, 45);
+        Employee[] employeeList = {
+            new Employee("Maria Popova", "Secretary", "maria96@mail.ru", 
+                "8-999-333-4554", 27000, 25), 
+            new Employee("Artem Fedorov", "Lawyer", "afedorov@mail.ru", 
+                "8-922-111-8787", 40000, 27), 
+            new Employee("Stanislav Sergeev", "Manager", "sergeev_stas@mail.ru", 
+                "8-912-322-6754", 32000, 23), 
+            new Employee("Aleksander Smirnov", "Senior manager", "SmirnovAA@gmail.ru", 
+                "8-999-777-8888", 50000, 37), 
+            new Employee("Igor Davidov", "Head of sales department", "DavidovIV76@mail.ru", 
+                "8-919-565-7878", 70000, 45)
+        };
 
-        for (int i = 0; i < 5; i++) {
-            if (employeeList[i].getAge() > 40) {
-                System.out.println(employeeList[i]);
+        for (Employee employee : employeeList) {
+            if (employee.getAge() > 40) {
+                System.out.println(employee);
             }    
         }
     }
@@ -44,6 +50,11 @@ class Employee {
 
     @Override
     public String toString() {
-        return "Name: " + employeename + "\nPosition: " + position + "\nE-mail: " + email + "\nPhone number: " + phonenumber + "\nSalary: " + salary + "\nAge: " + age + "\n";
+        return "Name: " + employeename + 
+            "\nPosition: " + position + 
+            "\nE-mail: " + email + 
+            "\nPhone number: " + phonenumber + 
+            "\nSalary: " + salary + 
+            "\nAge: " + age + "\n";
     }
 }
