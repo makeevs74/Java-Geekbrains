3c2e67c03cb02e3a0ba929ca31124d5f68c5d3cd
Sergey <82934913+makeevs74@users.noreply.github.com>
2021-11-25 04:32:21 +0500

    Update HomeWorkSeven.java

--- HomeWorkSeven.java
+++ HomeWorkSeven.java
@@ -16,7 +16,15 @@ class HomeWorkSeven {
 
         for (int i = 0; i < cat.length; i++) {
             System.out.println(cat[i]);
-
+            cat[i].eat(plate);
+            System.out.println(plate);
+            System.out.println(cat[i].name + " fullness is: " + cat[i].fullness);
+            System.out.println();
+        }
+        System.out.println("Добавим немного еды");
+        plate.addInPlate(20);
+        for (int i = 0; i < cat.length; i++) {
+            System.out.println(cat[i]);
             cat[i].eat(plate);
             System.out.println(plate);
             System.out.println(cat[i].name + " fullness is: " + cat[i].fullness);
@@ -29,6 +37,7 @@ class Cat {
     String name;
     int appetite;
     boolean fullness;
+    boolean foodIsEnough;
 
     Cat(String name, int appetite) {
         this.name = name;
@@ -38,28 +47,38 @@ class Cat {
 
     @Override
     public String toString() {
-        return "Cat: " + name + " eat " + appetite + " pieces";
+        return "Cat: " + name + " eats " + appetite + " pieces";
     }
 
-    void eat(Plate plate) {
-        if (plate.food >= appetite) {
+    boolean eat(Plate plate) {
+        if (fullness == false) {
             plate.decreaseFood(appetite);
-            fullness = true;
+            return fullness = true;
         } else {
-            fullness = false;
+            return fullness = false;
         }
     }
 }
 
-class Plate {;
+class Plate {
     int food;
+    boolean foodIsEnough;
 
     Plate(int food) {
         this.food = food;
     }
 
-    void decreaseFood(int food) {
-        this.food -= food;
+    public boolean decreaseFood(int amount) {
+        if (food >= amount) {
+            this.food = food - amount;
+            return foodIsEnough = true;
+        } else {
+            return foodIsEnough = false;
+        }
+    }
+
+    void addInPlate (int addFood) {
+        this.food += addFood;
     }
 
     @Override
